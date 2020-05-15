5f37bc23e9e442ff8ffab0ee968bacf3262ebe6c
luis <luispfa@gmail.com>
2020-05-15 18:41:57 +0200

    phpunit add void

--- tests/src/Shared/Infrastructure/PhpRandomNumberGeneratorTest.php
+++ tests/src/Shared/Infrastructure/PhpRandomNumberGeneratorTest.php
@@ -11,7 +11,7 @@ class PhpRandomNumberGeneratorTest extends TestCase
 {
 
     /** @test */
-    public function it_should_generate_a_random_number()
+    public function it_should_generate_a_random_number(): void
     {
         $generator = new PhpRandomNumberGenerator();
         $this->assertIsNumeric($generator->generate());
