9e8f7eb5838faa6ee3665b6a58ba25a0f1fe4611
luis <luispfa@gmail.com>
2020-05-15 18:38:48 +0200

    phpunit configured

---
+++ tests/src/Shared/Infrastructure/PhpRandomNumberGeneratorTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodelyTv\Tests\Shared\Infrastructure;
+
+use PHPUnit\Framework\TestCase;
+use CodelyTv\Shared\Infrastructure\PhpRandomNumberGenerator;
+
+class PhpRandomNumberGeneratorTest extends TestCase
+{
+
+    /** @test */
+    public function it_should_generate_a_random_number()
+    {
+        $generator = new PhpRandomNumberGenerator();
+        $this->assertIsNumeric($generator->generate());
+    }
+
+}
