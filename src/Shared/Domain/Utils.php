058d896f11a5ac10f1dd96afb7d5738ead93f9a2
luis <luispfa@gmail.com>
2020-06-09 17:55:11 +0200

    Add ability to test domain events from behat

--- src/Shared/Domain/Utils.php
+++ src/Shared/Domain/Utils.php
@@ -20,10 +20,17 @@ final class Utils
 
     public static function dateToString(DateTimeInterface $date) : string
     {
-        $timestamp = $date->getTimestamp();
-        $microseconds = $date->format('u');
-        $millisecondsOnASecond = 1000;
+        return $date->format(DateTimeInterface::ATOM);
+    }
 
-        return  (string) (((float) ($timestamp . '.' . $microseconds)) * $millisecondsOnASecond);
+    public static function jsonDecode(string $json): array
+    {
+        $data = json_decode($json, true);
+
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw new RuntimeException('Unable to parse response body into JSON: ' . json_last_error());
+        }
+
+        return $data;
     }
 }
